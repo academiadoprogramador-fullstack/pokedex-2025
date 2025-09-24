efa9cbaa7cfe07905b1155c50c27aa1856b1cc7a
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-09-24 08:51:40 -0300

    Implementada versão inicial com listagem

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,49 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+
+interface Pokemon {
+  nome: string;
+  urlSprite?: string;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('pokedex');
+export class App implements OnInit {
+  public pokemons: Pokemon[] = [];
+  private readonly url: string = "https://pokeapi.co/api/v2/pokemon/";
+
+  private readonly http = inject(HttpClient);
+
+  ngOnInit(): void {
+    this.http.get(this.url).subscribe((obj: any) => {
+      const arrayResultados: any[] = obj.results;
+
+      for (let resultado of arrayResultados) {
+        this.http.get(resultado.url).subscribe(objDetalhes => {
+          const pokemon = this.mapearPokemon(objDetalhes);
+
+          this.pokemons.push(pokemon);
+        })
+      }
+    });
+  }
+
+  private mapearPokemon(obj: any): Pokemon {
+    return { 
+      nome: this.converterParaTitleCase(obj.name),
+      urlSprite: obj.sprites.front_default
+    };
+  }
+
+  private converterParaTitleCase(texto: string): string {
+    if (texto.length < 1) return texto;
+
+    const novaString = texto[0].toUpperCase() + texto.substring(1).toLowerCase();
+
+    return novaString;
+  }
 }
