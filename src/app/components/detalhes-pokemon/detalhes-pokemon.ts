5d754a3dfaca17838f1168f94a78cd76899f7bae
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-09-29 09:33:00 -0300

    Adicionados tipos aos detalhes de pokémon

--- src/app/components/detalhes-pokemon/detalhes-pokemon.ts
+++ src/app/components/detalhes-pokemon/detalhes-pokemon.ts
@@ -9,50 +9,50 @@ import { mapeamentoDeCoresPorTipo } from '../../util/mapeamento-de-cores-por-tip
 @Component({
   selector: 'app-detalhes-pokemon',
   imports: [NgClass],
-  templateUrl: './detalhes-pokemon.html'
+  templateUrl: './detalhes-pokemon.html',
 })
 export class DetalhesPokemon implements OnInit {
   public detalhesDoPokemon?: DetalhesDoPokemon;
   public mapeamentoDeCoresPorTipo = mapeamentoDeCoresPorTipo;
 
-  private readonly url: string = "https://pokeapi.co/api/v2/pokemon";
+  private readonly url: string = 'https://pokeapi.co/api/v2/pokemon';
 
   private readonly http = inject(HttpClient);
   private readonly route = inject(ActivatedRoute);
 
   ngOnInit(): void {
-    const pokemonId = this.route.snapshot.paramMap.get("id");
+    const pokemonId = this.route.snapshot.paramMap.get('id');
 
-    const urlCompleto = `${this.url}/${pokemonId}`
+    const urlCompleto = `${this.url}/${pokemonId}`;
 
-    this.http.get(urlCompleto).subscribe(objDetalhes => {
+    this.http.get(urlCompleto).subscribe((objDetalhes) => {
       this.detalhesDoPokemon = this.mapearDetalhesDoPokemon(objDetalhes);
 
       console.log(this.detalhesDoPokemon);
-    })
+    });
   }
 
   private mapearDetalhesDoPokemon(obj: any): DetalhesDoPokemon {
     const sprites: string[] = [
-        obj.sprites.front_default,
-        obj.sprites.back_default,
-        obj.sprites.front_shiny,
-        obj.sprites.back_shiny,
-        obj.sprites.other.dream_world.front_default,
-        obj.sprites.other["official-artwork"].front_default,
+      obj.sprites.front_default,
+      obj.sprites.back_default,
+      obj.sprites.front_shiny,
+      obj.sprites.back_shiny,
+      obj.sprites.other.dream_world.front_default,
+      obj.sprites.other['official-artwork'].front_default,
     ];
 
-     return { 
+    return {
       id: obj.id,
       nome: converterParaTitleCase(obj.name),
       urlSprite: obj.sprites.front_default,
       tipos: obj.types.map((x: any) => converterParaTitleCase(x.type.name)),
       sons: this.mapearSonsDoPokemon(obj.cries),
-      sprites: sprites
+      sprites: sprites,
     };
   }
 
   private mapearSonsDoPokemon(obj: any): SonsDoPokemon {
-    return { atual: obj.latest, antigo: obj.legacy }
+    return { atual: obj.latest, antigo: obj.legacy };
   }
 }
