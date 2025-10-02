ac5f27574f7d54db4bdb12533c82a978bdadc01f
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-10-02 16:39:01 -0300

    Movido botão de alternar status de favorito, ativado dark mode

--- src/app/components/card-pokemon/card-pokemon.ts
+++ src/app/components/card-pokemon/card-pokemon.ts
@@ -10,36 +10,42 @@ import { RouterLink } from '@angular/router';
   template: `
     @if (pokemon) {
     <div class="card p-3 text-center">
-      @if (exibirLink) {
-      <a class="text-decoration-none" [routerLink]="['/pokemons', pokemon.id]">
+      <div class="card-body d-flex flex-column">
+        @if (exibirLink) {
+        <a class="text-decoration-none" [routerLink]="['/pokemons', pokemon.id]">
+          <h5 class="card-title">{{ pokemon.nome }}</h5>
+        </a>
+        } @else {
         <h5 class="card-title">{{ pokemon.nome }}</h5>
-      </a>
-      } @else {
-      <h5 class="card-title">{{ pokemon.nome }}</h5>
-      }
-
-      <div class="d-flex justify-content-center gap-2">
-        @for (tipo of pokemon.tipos; track $index) {
-        <span
-          class="badge rounded-pill text-light fs-6"
-          [ngClass]="mapeamentoDeCoresPorTipo[tipo]"
-          >{{ tipo }}</span
-        >
         }
+
+        <div class="d-flex justify-content-center gap-2">
+          @for (tipo of pokemon.tipos; track $index) {
+          <span
+            class="badge rounded-pill text-dark fs-6"
+            [ngClass]="mapeamentoDeCoresPorTipo[tipo]"
+            >{{ tipo }}</span
+          >
+          }
+        </div>
+
+        <img style="min-width: 200px;" [src]="pokemon.urlSprite" [alt]="pokemon.nome" />
+
+        <br />
+
+        <div class="app-container-alternar-status" title="Alternar Status de Favorito">
+          @if (pokemon.favorito) {
+          <button class="btn mx-auto" (click)="alternarStatusFavorito()">
+            <i class="bi bi-heart-fill fs-4 text-danger"></i>
+          </button>
+          } @else {
+
+          <button class="btn mx-auto" (click)="alternarStatusFavorito()">
+            <i class="bi bi-heart fs-4 text-danger"></i>
+          </button>
+          }
+        </div>
       </div>
-
-      <img [src]="pokemon.urlSprite" [alt]="pokemon.nome" />
-
-      @if (pokemon.favorito) {
-      <button class="btn mx-auto" (click)="alternarStatusFavorito()">
-        <i class="bi bi-heart-fill fs-4 text-danger"></i>
-      </button>
-      } @else {
-
-      <button class="btn mx-auto" (click)="alternarStatusFavorito()">
-        <i class="bi bi-heart fs-4 text-danger"></i>
-      </button>
-      }
     </div>
     }
   `,
