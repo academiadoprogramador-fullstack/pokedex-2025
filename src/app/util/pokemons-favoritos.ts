617aed85c0c90d14685283420967cea8cfece8aa
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-10-02 15:45:29 -0300

    Adicionado local-storage

--- src/app/util/pokemons-favoritos.ts
+++ src/app/util/pokemons-favoritos.ts
@@ -1,13 +1,25 @@
 import { Pokemon } from '../models/pokemon';
 
-export let pokemonsFavoritos: Pokemon[] = [];
+export const pokemonsFavoritos: Pokemon[] = obterPokemonsFavoritos();
 
-export function alternarStatusPokemon(pokemon: Pokemon) {
+export function alternarStatusPokemon(pokemon: Pokemon): void {
   if (pokemon.favorito) {
     pokemon.favorito = false;
-    pokemonsFavoritos = pokemonsFavoritos.filter((x) => x.id != pokemon.id);
+
+    const index = pokemonsFavoritos.findIndex((x) => x.id === pokemon.id);
+    if (index > -1) pokemonsFavoritos.splice(index, 1);
   } else {
     pokemon.favorito = true;
     pokemonsFavoritos.push(pokemon);
   }
+
+  localStorage.setItem('pokedex:favoritos', JSON.stringify(pokemonsFavoritos));
+}
+
+function obterPokemonsFavoritos(): Pokemon[] {
+  var itensArmazenados = localStorage.getItem('pokedex:favoritos');
+
+  if (!itensArmazenados) return [];
+
+  return JSON.parse(itensArmazenados) as Pokemon[];
 }
